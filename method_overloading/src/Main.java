7614fc00c142f92968a819c532192eb4e1c3c982
Himanshu Sharma <himanshu@raftlabs.co>
2022-05-05 13:47:29 +0530

    Add method overloading example

---
+++ method_overloading/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        fun("abcd");
+        fun(123);
+    }
+
+    static void fun(int number) {
+        System.out.println(number + " is a Number !");
+    }
+
+    static void fun(String string) {
+        System.out.println(string + " is a String !");
+    }
+}
