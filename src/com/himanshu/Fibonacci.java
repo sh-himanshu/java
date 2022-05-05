17abea3832aa3bbafe1adb1c7b403c922badc762
Himanshu Sharma <himanshu@raftlabs.co>
2022-05-05 16:13:36 +0530

    Refactor project

--- src/com/himanshu/Fibonacci.java
+++ src/com/himanshu/Fibonacci.java
@@ -1,16 +1,16 @@
+package com.himanshu;
+
 import java.util.Scanner;
 
-// Fibonacci series -> 0, 1, 1, 2, 3, 5, 8, 13 ...
-
-public class Main {
+public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of 'N': ");
+        System.out.print("Enter the value of 'n': ");
         int n = sc.nextInt();
-        System.out.println(new Main().genFibonacci(n));
+        System.out.println(n + " term of Fibonacci series is: " + genFibonacci(n));
     }
 
-    public int genFibonacci(int number) {
+    static int genFibonacci(int number) {
         if (number == 0 || number == 1) return number;
         return genFibonacci((number - 1)) + genFibonacci((number - 2));
     }
--- src/com/himanshu/InputOutput.java
+++ src/com/himanshu/InputOutput.java
@@ -1,6 +1,8 @@
+package com.himanshu;
+
 import java.util.Scanner;
 
-public class Main {
+public class InputOutput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Type a number: ");
